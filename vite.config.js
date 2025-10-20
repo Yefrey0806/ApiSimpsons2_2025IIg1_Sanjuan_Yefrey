0b2060a7539c4205d43124ddaca12fd8f3279bf6
Yefrey0806 <sanjuanferia@gmail.com>
2025-10-19 22:30:03 -0500

    V1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/ApiTheSimpsons_2025IIg1_Sanjuan_Yefrey_2/',
+  base:'/ApiTheSimpsons_2025IIg1_Sanjuan_Yefrey/',
 })
